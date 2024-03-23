eda3520315770b4d0fee023b4bb5d66cc1c5d2a4
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-03-23 09:21:31 +0100

    chore: solve merge conflict with gitignore

---
+++ src/test/java/edu/ntnu/idatt2105/quizapp/QuizBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package edu.ntnu.idatt2105.quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+
+  }
+
+}
