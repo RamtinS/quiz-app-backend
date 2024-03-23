858f1240930b6eb8e1ae4ee8c5d65fc01b1a479d
tabir <tabirijeffrey@gmail.com>
2024-03-23 18:57:10 +0100

    chore: disable cross origin

--- src/main/java/edu/ntnu/idatt2105/quizapp/controller/AuthenticationController.java
+++ src/main/java/edu/ntnu/idatt2105/quizapp/controller/AuthenticationController.java
@@ -10,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *  Controller class responsible for handling user authentication operations
@@ -27,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin("*")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
