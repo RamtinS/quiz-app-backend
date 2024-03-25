dd49f8ddf0b5bdc0ba19ca7bde740e5b8c0d3c61
tabir <tabirijeffrey@gmail.com>
2024-03-25 17:08:27 +0100

    refactor(Role): refactor role to enum

--- src/main/java/edu/ntnu/idatt2105/quizapp/model/User.java
+++ src/main/java/edu/ntnu/idatt2105/quizapp/model/User.java
@@ -1,20 +1,13 @@
 package edu.ntnu.idatt2105.quizapp.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.util.Collection;
-import java.util.Set;
+import java.util.List;
 
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 /**
@@ -45,7 +38,6 @@ public class User implements UserDetails {
   @NonNull
   private String username;
 
-
   @Column(name = "password", nullable = false)
   @NonNull
   private String password;
@@ -59,16 +51,10 @@ public class User implements UserDetails {
   @Column(name = "last_name")
   private String surName;
 
-  /**
-   * A many-to-many relational table which tracks the roles/authorities associated with the user.
-   */
-  @ManyToMany(fetch = FetchType.EAGER)
-  @JoinTable(
-          name = "user_role_junction",
-          joinColumns = {@JoinColumn(name = "user_id")},
-          inverseJoinColumns = {@JoinColumn(name = "role_id")}
-  )
-  private Set<Role> authorities;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "role", nullable = false)
+  @NonNull
+  private Role role;
 
   /**
    * Retrieves the roles/authorities associated with this user.
@@ -77,7 +63,7 @@ public class User implements UserDetails {
    */
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return this.authorities;
+    return List.of(new SimpleGrantedAuthority(role.name()));
   }
 
   @Override
