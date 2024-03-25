8cd926c3440147dc65cdeabf00aba2c548667b60
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-03-25 11:32:33 +0100

    Add javadoc in JwtAuthenticationFilter

--- src/main/java/edu/ntnu/idatt2105/quizapp/config/JwtAuthenticationFilter.java
+++ src/main/java/edu/ntnu/idatt2105/quizapp/config/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package edu.ntnu.idatt2105.quizapp.config;
 
-
 import edu.ntnu.idatt2105.quizapp.services.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -10,59 +9,71 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
 
+/**
+ * The class is responsible for authenticating JWT in incoming HTTP requests.
+ *
+ * @author Ramtin samavat
+ * @author Jeffrey Tabiri
+ * @version 1.0
+ * @since 2024-03-25
+ */
+@Slf4j
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
+  private final JwtService jwtService;
+  private final UserDetailsService userDetailsService;
 
+  /**
+   * Checks for JWT in the Authorization header and authenticates the user.
+   *
+   * @param request HTTP request.
+   * @param response HTTP response.
+   * @param filterChain Filter chain for the request.
+   * @throws ServletException if an error occurs during the filter processing.
+   * @throws IOException if an I/O error occurs.
+   */
+  @Override
+  protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+                                  @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-    @Override
-    protected void doFilterInternal(
-            @NonNull HttpServletRequest request,
-            @NonNull HttpServletResponse response,
-            @NonNull FilterChain filterChain
-    ) throws ServletException, IOException {
+    final String authHeader = request.getHeader("Authorization");
+    final String token;
+    final String username;
 
-        final String authHeader = request.getHeader("Authorization");
-        final String token;
-        final String username;
-
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        token = authHeader.substring(7);
-        username = jwtService.extractUsername(token);
-
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-            if (jwtService.isTokenValid(token, userDetails)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails,
-                        null,
-                        userDetails.getAuthorities()
-                );
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
-                SecurityContextHolder.getContext().setAuthentication(authToken);
-            }
-        }
-        filterChain.doFilter(request, response);
+    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+      filterChain.doFilter(request, response);
+      return;
     }
+
+    token = authHeader.substring(7);
+    log.info("JWT received through the HTTP request: {}", token);
+
+    username = jwtService.extractUsername(token);
+
+    if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+      UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+      if (jwtService.isTokenValid(token, userDetails)) {
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities());
+
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+      }
+    }
+    filterChain.doFilter(request, response);
+  }
 }
