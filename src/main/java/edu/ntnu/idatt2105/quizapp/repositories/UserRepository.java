1d3168ed3bd3a5486300e21afbab0c059fb513b5
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-03-25 12:19:14 +0100

    Refactor UserRepository to extend JpaRepository

--- src/main/java/edu/ntnu/idatt2105/quizapp/repositories/UserRepository.java
+++ src/main/java/edu/ntnu/idatt2105/quizapp/repositories/UserRepository.java
@@ -1,9 +1,8 @@
 package edu.ntnu.idatt2105.quizapp.repositories;
 
 import edu.ntnu.idatt2105.quizapp.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 /**
@@ -11,11 +10,11 @@ import java.util.Optional;
  * The interface extends JpaRepository.
  * The class allows for basic CRUD (Create, Read, Update, Delete) operations on users.
  *
+ * @author Jeffrey Tabiri
  * @version 1.0
  * @since 2024-03-22
- * @author Jytabiri
  */
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findUserByUsername(String username);
 }
