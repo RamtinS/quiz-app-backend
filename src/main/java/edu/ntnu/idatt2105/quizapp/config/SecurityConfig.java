4aa45e72130f59a20c0d36631767157af366987a
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-03-26 17:16:13 +0100

    Add CORS configuration to SecurityConfig
    Integrate CORS configuration to define allowed origins, methods, headers, and credentials for HTTP requests.

--- src/main/java/edu/ntnu/idatt2105/quizapp/config/SecurityConfig.java
+++ src/main/java/edu/ntnu/idatt2105/quizapp/config/SecurityConfig.java
@@ -4,13 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
 
 /**
  * Security configuration class responsible for defining security
@@ -32,7 +36,7 @@ public class SecurityConfig {
   private final AuthenticationProvider authenticationProvider;
 
   /**
-   * This method defines a filter chain that specifies how Spring Security handles
+   * The method defines a filter chain that specifies how Spring Security handles
    * each incoming HTTP request.
    *
    * @param http The HttpSecurity object representing the HTTP security configuration.
@@ -41,15 +45,34 @@ public class SecurityConfig {
    */
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http
-            .cors(AbstractHttpConfigurer::disable)
+    http.cors(Customizer.withDefaults())
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers(WHITELIST_URL).permitAll()
                     .anyRequest().authenticated())
             .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authenticationProvider(authenticationProvider)
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-            .build();
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
+  }
+
+  /**
+   * The method configure CORS (Cross-Origin Resource Sharing) for the application.
+   * This method defines allowed origins, methods, headers, and credentials for cross-origin requests.
+   *
+   * @return CorsConfigurationSource with configured CORS settings.
+   */
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    final CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of("http://localhost:3000"));
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+    config.setAllowCredentials(true);
+
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config); // Apply the CORS configuration to all paths.
+    return source;
   }
 }
