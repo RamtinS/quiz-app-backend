04fd401ba623ac9c72228f47435a194ee2b5a02f
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-03-28 11:13:59 +0100

    Remove unused import from AuthenticationService

--- src/main/java/edu/ntnu/idatt2105/quizapp/services/AuthenticationService.java
+++ src/main/java/edu/ntnu/idatt2105/quizapp/services/AuthenticationService.java
@@ -12,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
