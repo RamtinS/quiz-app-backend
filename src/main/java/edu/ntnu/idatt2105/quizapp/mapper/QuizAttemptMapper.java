e34ec6b59c667de8c7c906cf8a640f5ff7667936
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-04 22:14:24 +0200

    Refactor QuizAttemptMapper
    Remove methods that cannot be used.

--- src/main/java/edu/ntnu/idatt2105/quizapp/mapper/QuizAttemptMapper.java
+++ src/main/java/edu/ntnu/idatt2105/quizapp/mapper/QuizAttemptMapper.java
@@ -1,36 +1,15 @@
 package edu.ntnu.idatt2105.quizapp.mapper;
 
 import edu.ntnu.idatt2105.quizapp.dto.quiz.QuizAttemptDto;
-import edu.ntnu.idatt2105.quizapp.model.User;
-import edu.ntnu.idatt2105.quizapp.model.quiz.QuizAttempt;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
+@Component
 public class QuizAttemptMapper {
 
-  /**
-   * Maps a QuizAttempt object to an QuizAttemptDto object.
-   *
-   * @param quizAttempt is the quiz attempt we would want to convert
-   * @return The mapped quizAttemptDto object.
-   */
-  public QuizAttemptDto mapToQuizAttemptDto(QuizAttempt quizAttempt) {
-    return QuizAttemptDto.builder()
-            .score(quizAttempt.getScore())
-            .date(quizAttempt.getAttemptDate())
-            .username(quizAttempt.getUser().getUsername())
-            .build();
-  }
+  public void mapQuizAttempt(QuizAttemptDto quizAttemptDto) {
 
-  /**
-   * Maps a QuizAttemptDto object to an QuizAttempt object.
-   *
-   * @param quizAttemptDto The quizAttemptDto object to map.
-   * @return The mapped tag object.
-   */
-  public QuizAttempt mapToQuizAttempt(QuizAttemptDto quizAttemptDto, User user) {
-    return QuizAttempt.builder()
-            .score(quizAttemptDto.getScore())
-            .attemptDate(quizAttemptDto.getDate())
-            .user(user)
-            .build();
   }
 }
