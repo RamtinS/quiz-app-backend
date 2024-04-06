0242454b651b2457e845d06028e52486ca1108a9
Tobias <oftedal03@gmail.com>
2024-04-06 20:27:07 +0200

    Replace tag repository with CategoryRepository

--- src/main/java/edu/ntnu/idatt2105/quizapp/repositories/quiz/CategoryRepository.java
+++ src/main/java/edu/ntnu/idatt2105/quizapp/repositories/quiz/CategoryRepository.java
@@ -1,6 +1,10 @@
 package edu.ntnu.idatt2105.quizapp.repositories.quiz;
 
 import edu.ntnu.idatt2105.quizapp.model.quiz.Category;
+import java.util.Optional;
+import lombok.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -13,4 +17,7 @@ import java.util.List;
  */
 public interface CategoryRepository extends JpaRepository<Category, Long> {
   List<Category> findCategoriesByDescription(String description);
+
+  Optional<Category> findCategoryByDescription(String description);
+
 }
