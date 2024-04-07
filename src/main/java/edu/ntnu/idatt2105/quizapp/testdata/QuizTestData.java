8ef20341f43f8af76cf301ced43f58c4bbdbe47b
Tobias <oftedal03@gmail.com>
2024-04-07 20:54:31 +0200

    Add new quiz test data

--- src/main/java/edu/ntnu/idatt2105/quizapp/testdata/QuizTestData.java
+++ src/main/java/edu/ntnu/idatt2105/quizapp/testdata/QuizTestData.java
@@ -6,7 +6,6 @@ import edu.ntnu.idatt2105.quizapp.model.quiz.Answer;
 import edu.ntnu.idatt2105.quizapp.model.quiz.Category;
 import edu.ntnu.idatt2105.quizapp.model.quiz.MultipleChoiceQuestion;
 import edu.ntnu.idatt2105.quizapp.model.quiz.Quiz;
-import edu.ntnu.idatt2105.quizapp.model.quiz.Tag;
 import edu.ntnu.idatt2105.quizapp.model.quiz.QuizAttempt;
 import edu.ntnu.idatt2105.quizapp.model.quiz.TrueOrFalseQuestion;
 import edu.ntnu.idatt2105.quizapp.repositories.UserRepository;
@@ -14,211 +13,244 @@ import edu.ntnu.idatt2105.quizapp.repositories.quiz.CategoryRepository;
 import edu.ntnu.idatt2105.quizapp.repositories.quiz.QuizAttemptRepository;
 import edu.ntnu.idatt2105.quizapp.repositories.quiz.QuizRepository;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import lombok.Generated;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * Class for adding test data to the database.
+ * Should not be included in production code.
+ * Should not be seen as a part of the testing profile
+ *
+ * @author Tobias Oftedal
+ */
+@Slf4j
 public class QuizTestData {
 
-  public static void addTestData(PasswordEncoder passwordEncoder,
-                                 UserRepository userRepository,
+  /**
+   * List of possible categories for the quizzes.
+   */
+  private static final String[] possibleCategories = {
+      "Miscellaneous", "Food", "Sports", "Gaming", "Math", "Physics", "Chemistry"
+  };
+
+  /**
+   * Adds test data to the database.
+   *
+   * @param userRepository        The user repository
+   * @param passwordEncoder       The password encoder
+   * @param quizRepository        The quiz repository
+   * @param categoryRepository    The category repository
+   * @param quizAttemptRepository The quiz attempt repository
+   */
+  public static void addTestData(UserRepository userRepository, PasswordEncoder passwordEncoder,
                                  QuizRepository quizRepository,
                                  CategoryRepository categoryRepository,
                                  QuizAttemptRepository quizAttemptRepository) {
+    try {
+      List<Category> categories = createAndSaveCategories(categoryRepository);
+      List<User> users = createTestUsers(passwordEncoder, userRepository);
+      addCypressUser(passwordEncoder, userRepository);
+      users.add(addAdminUser(passwordEncoder, userRepository));
+      for (int i = 0; i < users.size(); i++) {
+        User currentUser = users.get(i);
 
-    Category exampleCategory = Category.builder().description("Food").build();
-    Category exampleCategory2 = Category.builder().description("Sports").build();
-    Category exampleCategory3 = Category.builder().description("Gaming").build();
-    Category exampleCategory4 = Category.builder().description("Math").build();
-    Category exampleCategory5 = Category.builder().description("Physics").build();
-    Category exampleCategory6 = Category.builder().description("Chemistry").build();
+        Category category = categories.get(i % categories.size());
 
-    User admin = User.builder()
-            .username("admin")
-            .password(passwordEncoder.encode("password"))
-            .email("admin@ntnu.edu")
-            .name("Test")
-            .surName("Test")
-            .role(Role.ADMIN)
-            .build();
+        Quiz quiz = createQuiz(category);
+
+        currentUser.setQuizzes(List.of(quiz));
+        quiz.setAuthor(currentUser);
 
 
-    User user2 = User.builder()
-            .username("geir1")
-            .password(passwordEncoder.encode("password"))
-            .email("geir1@ntnu.edu")
-            .name("Test")
-            .surName("Test")
-            .role(Role.USER)
-            .build();
+        quizRepository.save(quiz);
+        userRepository.save(currentUser);
 
-    User emptyUser = User.builder() // Do not change. Is used for testing in frontend.
-            .username("testUser")
-            .password(passwordEncoder.encode("password"))
-            .email("test@test.no")
-            .name("Test")
-            .surName("Test")
-            .role(Role.USER)
-            .build();
-
-    userRepository.save(admin);
-    userRepository.save(user2);
-    userRepository.save(emptyUser);
-
-
-
-    categoryRepository.save(exampleCategory);
-    categoryRepository.save(exampleCategory2);
-    categoryRepository.save(exampleCategory3);
-    categoryRepository.save(exampleCategory4);
-    categoryRepository.save(exampleCategory5);
-    categoryRepository.save(exampleCategory6);
-
-    addTestDataToUser(admin, quizRepository, categoryRepository, quizAttemptRepository);
-    addTestDataToUser(user2, quizRepository, categoryRepository, quizAttemptRepository);
-
-
-  }
-
-  public static void addTestDataToUser(User user, QuizRepository quizRepository,
-                                       CategoryRepository categoryRepository,
-                                       QuizAttemptRepository quizAttemptRepository) {
-
-
-    categoryRepository.save(categoryRepository.findCategoryByDescription("Food").get());
-    categoryRepository.save(categoryRepository.findCategoryByDescription("Sports").get());
-    categoryRepository.save(categoryRepository.findCategoryByDescription("Gaming").get());
-    int randomIntBetween20And30 = (int) (Math.random() * 20) + 10;
-    for (int i = 0; i < 1; i++) {
-
-      Quiz quiz = Quiz.builder()
-              .name("Example quiz")
-              .description("random desc: " + (randomIntBetween20And30 + i))
-              .author(user)
-              .isOpen(true)
-              .build();
-
-      Quiz savedQuiz = quizRepository.save(quiz);
-
-      MultipleChoiceQuestion multipleChoiceQuestion = MultipleChoiceQuestion.builder()
-              .quiz(quiz)
-              .questionText("What is the capital of Norway?")
-              .build();
-
-      multipleChoiceQuestion.setQuiz(quiz);
-
-
-      TrueOrFalseQuestion trueOrFalseQuestion = TrueOrFalseQuestion.builder()
-              .quiz(quiz)
-              .questionText("Is the earth flat?")
-              .questionIsCorrect(false)
-              .build();
-
-      trueOrFalseQuestion.setQuiz(quiz);
-
-      quiz.setQuestions(List.of(multipleChoiceQuestion, trueOrFalseQuestion));
-
-
-
-      List<Answer> exampleAnswers = List.of(
-              Answer.builder()
-                      .question(multipleChoiceQuestion)
-                      .answerText("Oslo")
-                      .isCorrect(true)
-                      .build(),
-              Answer.builder()
-                      .answerText("Bergen")
-                      .question(multipleChoiceQuestion)
-                      .isCorrect(false)
-                      .build(),
-              Answer.builder()
-                      .answerText("Trondheim")
-                      .question(multipleChoiceQuestion)
-
-                      .isCorrect(false)
-                      .build(),
-              Answer.builder()
-                      .answerText("Stavanger")
-                      .question(multipleChoiceQuestion)
-                      .isCorrect(false)
-                      .build()
-      );
-
-      multipleChoiceQuestion.setAnswers(exampleAnswers);
-      exampleAnswers.forEach(answer -> answer.setQuestion(multipleChoiceQuestion));
-
-      List<Tag> exampleTag = List.of(
-              Tag.builder().description("Mathematical").build(),
-              Tag.builder().description("Physics").build()
-      );
-
-      quiz.setCategory(categoryRepository.findCategoryByDescription("food").get());
-      quiz.setTags(exampleTag);
-      quizRepository.save(quiz);
-
-
-      Long id = quizRepository.save(quiz).getId();
-      Quiz quiz1 = quizRepository.findQuizById(id).get();
-
-
-
-      //Quiz attempts.
-      QuizAttempt quizAttempt = QuizAttempt.builder()
-              .quiz(quiz)
-              .user(user)
-              .score(2)
-              .timestamp(LocalDate.now())
-              .build();
-
-      QuizAttempt quizAttempt1 = QuizAttempt.builder()
-              .quiz(quiz)
-              .user(user)
-              .score(2)
-              .timestamp(LocalDate.now())
-              .build();
-
-      QuizAttempt quizAttempt2 = QuizAttempt.builder()
-              .quiz(quiz)
-              .user(user)
-              .score(2)
-              .timestamp(LocalDate.of(2024, 4, 3))
-              .build();
-
-      QuizAttempt quizAttempt3 = QuizAttempt.builder()
-              .quiz(quiz)
-              .user(user)
-              .score(2)
-              .timestamp(LocalDate.of(2024, 4, 2))
-              .build();
-
-      QuizAttempt quizAttempt4 = QuizAttempt.builder()
-              .quiz(quiz)
-              .user(user)
-              .score(2)
-              .timestamp(LocalDate.of(2024, 4, 1))
-              .build();
-
-      QuizAttempt quizAttempt5 = QuizAttempt.builder()
-              .quiz(quiz)
-              .user(user)
-              .score(2)
-              .timestamp(LocalDate.of(2024, 3, 31))
-              .build();
-
-      QuizAttempt quizAttempt6 = QuizAttempt.builder()
-              .quiz(quiz)
-              .user(user)
-              .score(2)
-              .timestamp(LocalDate.of(2024, 3, 30))
-              .build();
-
-      quizAttemptRepository.save(quizAttempt5);
-      quizAttemptRepository.save(quizAttempt6);
-      quizAttemptRepository.save(quizAttempt);
-      quizAttemptRepository.save(quizAttempt1);
-      quizAttemptRepository.save(quizAttempt2);
-      quizAttemptRepository.save(quizAttempt3);
-      quizAttemptRepository.save(quizAttempt4);
+        List<QuizAttempt> quizAttempts = createQuizAttempts();
+        quizAttempts.forEach(quizAttempt -> {
+          quizAttempt.setUser(currentUser);
+          quizAttempt.setQuiz(quiz);
+          quizAttemptRepository.save(quizAttempt);
+        });
+      }
+    } catch (Exception e) {
+      log.error("Error adding test data: ", e);
     }
   }
+
+  /**
+   * Creates and saves categories to the database.
+   *
+   * @param categoryRepository The category repository
+   * @return A list of the created categories
+   */
+  public static List<Category> createAndSaveCategories(CategoryRepository categoryRepository) {
+    List<Category> categories = new ArrayList<>();
+    for (String category : possibleCategories) {
+      Category createdCategory = Category.builder().description(category).build();
+      if (categoryRepository.findCategoryByDescription(category).isEmpty()) {
+        Category savedCategory = categoryRepository.save(createdCategory);
+        categories.add(savedCategory);
+      }
+    }
+    return categories;
+  }
+
+  /**
+   * Creates test users and saves them to the database.
+   *
+   * @param passwordEncoder The password encoder
+   * @param userRepository  The user repository
+   * @return A list of the created users
+   */
+  private static List<User> createTestUsers(PasswordEncoder passwordEncoder,
+                                            UserRepository userRepository) {
+    ArrayList<User> users = new ArrayList<>();
+
+    for (int i = 1; i < 10; i++) {
+      User user = User.builder()
+          .username("user" + i).password(passwordEncoder.encode("password"))
+          .email("user" + i + "@ntnu.no").name("Test").surName("Test").role(Role.USER)
+          .quizzes(new ArrayList<>())
+          .build();
+      if (userRepository.findUserByUsernameIgnoreCase(user.getUsername()).isEmpty()) {
+        User savedUser = userRepository.save(user);
+        users.add(savedUser);
+      }
+    }
+    return users;
+  }
+
+  /**
+   * Adds an admin user to the database.
+   *
+   * @param passwordEncoder The password encoder
+   * @param userRepository  The user repository
+   * @return The created admin user
+   */
+  private static User addAdminUser(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+    User admin = User.builder()
+        .username("admin").password(passwordEncoder.encode("password"))
+        .email("admin@ntnu.edu").name("Test").surName("Test").role(Role.ADMIN)
+        .build();
+    userRepository.save(admin);
+    return admin;
+  }
+
+  /**
+   * Adds a cypress user to the database.
+   *
+   * @param passwordEncoder The password encoder
+   * @param userRepository  The user repository
+   */
+  private static void addCypressUser(PasswordEncoder passwordEncoder,
+                                     UserRepository userRepository) {
+    User emptyUser = User.builder() // Do not change. Is used for testing in frontend.
+        .username("testUser")
+        .password(passwordEncoder.encode("password"))
+        .email("test@test.no")
+        .name("Test")
+        .surName("Test")
+        .role(Role.USER)
+        .build();
+    userRepository.save(emptyUser);
+  }
+
+  /**
+   * Creates a quiz with questions and 1 answer of each type.
+   *
+   * @param category The category of the quiz
+   * @return The created quiz
+   */
+  private static Quiz createQuiz(Category category) {
+
+    Quiz quiz = Quiz.builder()
+        .name("Test quiz")
+        .description("This is a test quiz")
+        .isOpen(true)
+        .category(category)
+        .build();
+
+    MultipleChoiceQuestion multipleChoiceQuestion = createMultipleChoiceQuestion();
+    TrueOrFalseQuestion trueOrFalseQuestion = createTrueOrFalseQuestion();
+
+    multipleChoiceQuestion.setQuiz(quiz);
+    trueOrFalseQuestion.setQuiz(quiz);
+    quiz.setQuestions(List.of(multipleChoiceQuestion, trueOrFalseQuestion));
+    return quiz;
+  }
+
+  /**
+   * Creates a multiple choice question with 4 answers.
+   *
+   * @return The created multiple choice question
+   */
+  private static MultipleChoiceQuestion createMultipleChoiceQuestion() {
+    MultipleChoiceQuestion multipleChoiceQuestion = MultipleChoiceQuestion.builder()
+        .questionText("Where is Gløshaugen located?")
+        .build();
+
+    List<Answer> answers = List.of(
+        Answer.builder()
+            .question(multipleChoiceQuestion)
+            .answerText("Oslo")
+            .isCorrect(false)
+            .build(),
+        Answer.builder()
+            .question(multipleChoiceQuestion)
+            .answerText("Bergen")
+            .isCorrect(false)
+            .build(),
+        Answer.builder()
+            .question(multipleChoiceQuestion)
+            .answerText("Trondheim")
+            .isCorrect(true)
+            .build(),
+        Answer.builder()
+            .question(multipleChoiceQuestion)
+            .answerText("Stavanger")
+            .isCorrect(false)
+            .build()
+    );
+
+    answers.forEach(answer -> answer.setQuestion(multipleChoiceQuestion));
+    multipleChoiceQuestion.setAnswers(answers);
+
+    return multipleChoiceQuestion;
+  }
+
+  /**
+   * Creates a true or false question.
+   *
+   * @return The created true or false question
+   */
+  private static TrueOrFalseQuestion createTrueOrFalseQuestion() {
+    return TrueOrFalseQuestion.builder()
+        .questionText("The sky is blue")
+        .questionIsCorrect(true)
+        .build();
+
+  }
+
+  /**
+   * Creates 20 quiz attempts with random scores and timestamps from the last week.
+   *
+   * @return A list of the created quiz attempts
+   */
+  private static List<QuizAttempt> createQuizAttempts() {
+    List<QuizAttempt> quizAttempts = new ArrayList<>();
+    for (int i = 0; i < 20; i++) {
+      QuizAttempt quizAttempt = QuizAttempt.builder()
+          .score(i * i - 3 * i + 3)
+          .timestamp(LocalDate.now().minusDays(new Random().nextInt(7) + 1))
+          .build();
+      quizAttempts.add(quizAttempt);
+    }
+    return quizAttempts;
+  }
+
 
 }
